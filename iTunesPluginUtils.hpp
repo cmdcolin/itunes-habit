0f8b868a77eee4aa83fcedcb0e870d6451250193
colin.diesh <colin.diesh@37d2c1db-bb2f-0410-a901-09a4849b322f>
2007-05-07 01:24:58 +0000

    Moved remotely
    git-svn-id: http://zxcvbn.googlecode.com/svn/trunk/habit@18 37d2c1db-bb2f-0410-a901-09a4849b322f

---
+++ iTunesPluginUtils.hpp
@@ -0,0 +1,159 @@
+#pragma once
+
+#include <xercesc/dom/DOMException.hpp>
+#include <curl/curl.h>
+
+#include "iTunesAPI.hpp"
+#include "iTunesVisualAPI.hpp"
+
+#include "dbgUtils.hpp"
+#include "xmlLog.hpp"
+#include "imgCreator.hpp"
+#include "httpUploader.hpp"
+
+#include <ctime>
+#include <string>
+#include <vector>
+
+#include <tchar.h>
+
+
+struct
+    VisualPluginData
+{
+    VisualPluginData(
+        const std::basic_string<TCHAR> & mlog,
+        const std::basic_string<TCHAR> & imgfike,
+        const std::basic_string<TCHAR> & grammar,
+        const std::basic_string<TCHAR> & elog
+        ) :
+
+    error_log_file(elog), music_log_file(mlog),
+        img_file(imgfike), grammar_file(grammar)
+
+    {
+        BOOST_LOGL(app, info) << __FUNCTION__;
+
+        try
+        {
+            HANDLE file = ::CreateFile(mlog.c_str(), GENERIC_READ,
+                FILE_SHARE_READ, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+
+            if(file == INVALID_HANDLE_VALUE &&
+                ::GetLastError() == ERROR_FILE_NOT_FOUND)
+            {
+                loggy = new Log(grammar_file.c_str(), false);
+            }
+            else
+            {
+                ::CloseHandle(file);
+                loggy = new Log(music_log_file.c_str(), true);
+            }
+        }
+        catch(XMLException & e)
+        {
+            BOOST_LOG(app) << SX(e.getMessage());
+        }
+        catch(DOMException & e)
+        {
+            BOOST_LOG(app) << SX(e.getMessage());
+        }
+    }
+
+    ~VisualPluginData()
+    {
+        BOOST_LOGL(app, info) << __FUNCTION__;
+
+        delete loggy;
+    }
+
+	void *			cookie;
+	ITAppProcPtr	proc;
+
+	ITTrackInfoV1	trackInfo;
+	ITStreamInfoV1	streamInfo;
+
+    ITTrackInfo     trackUniInfo;
+    ITStreamInfo    streamUniInfo;
+
+	Boolean	playing;
+
+    Log *     loggy;
+    Creator   ic;
+    Uploader  up;
+
+    std::basic_string<TCHAR> host;
+    std::basic_string<TCHAR> music_log_file;
+    std::basic_string<TCHAR> error_log_file;
+    std::basic_string<TCHAR> img_file;
+    std::basic_string<TCHAR> grammar_file;
+
+    HWND dialog;
+    HWND itunes;
+
+    DWORD tid;
+    HANDLE p;
+
+    Rect r;
+
+};
+
+//
+// Deprecated - Functor converts type U to type vector of T's. Work on this
+//
+
+template <typename T>
+    struct
+        converter
+{
+    std::vector<T> r;
+
+    template <typename U>
+        void operator () (U u)
+    {
+        r.push_back(static_cast<T>(u));
+    }
+
+    T * str(void)
+    {
+        if(r.back() != 0)
+            r.push_back(0);
+
+        return &r.at(0);
+    }
+};
+
+unsigned int seed();
+bool normalizeCurrentDirectory();
+
+static OSStatus VisualPluginHandler(
+                                    OSType message,
+                                    VisualPluginMessageInfo * messageInfo,
+                                    void * refCon
+                                    );
+
+
+
+DWORD WINAPI ThreadProc(LPVOID t);
+
+
+
+BOOL CALLBACK DlgProc(
+                      HWND dlg,
+                      UINT message,
+                      WPARAM wParam,
+                      LPARAM lParam
+                      );
+
+extern "C" __declspec(dllexport) BOOL WINAPI DllMain(
+    HINSTANCE /* instance */,
+    DWORD reason,
+    LPVOID /* reserved */
+    );
+
+
+extern "C" __declspec(dllexport) OSStatus iTunesPluginMain(
+    OSType message,
+    PluginMessageInfo * messageInfo,
+    void * /* refCon */
+    );
