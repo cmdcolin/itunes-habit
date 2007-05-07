d140ecd7ec760e2a245688cb78c10bff4e708ccf
colin.diesh <colin.diesh@37d2c1db-bb2f-0410-a901-09a4849b322f>
2007-05-07 01:23:53 +0000

    Moved remotely
    git-svn-id: http://zxcvbn.googlecode.com/svn/trunk/habit@14 37d2c1db-bb2f-0410-a901-09a4849b322f

---
+++ iTunesPluginUtils.cpp
@@ -0,0 +1,52 @@
+#include "iTunesPluginUtils.hpp"
+
+
+unsigned int seed()
+{
+    time_t now = std::time(0);
+
+    unsigned char * p = reinterpret_cast<unsigned char *>(&now);
+    unsigned int seed = 0;
+
+    for(unsigned int i = 0; i < sizeof(now); ++i)
+        seed = seed * (UCHAR_MAX + 2U) + p[i];
+
+    return seed;
+}
+
+// Returns false on failure
+// More info in GetLastError
+
+bool normalizeCurrentDirectory()
+{
+    std::vector<TCHAR> v(256);
+    DWORD ret = 0;
+    
+    ret = ::GetModuleFileName(0, &v[0], static_cast<DWORD>(v.size()));
+
+    if(ret == 0)
+    {
+        BOOST_LOGL(app, info) << "Failed at GetModuleFileName";
+        return false;
+    }
+
+    std::basic_string<TCHAR> s(v.begin(), v.end());
+
+    ret = ::SetCurrentDirectory(s.substr(0, s.rfind('\\')).c_str());
+
+    if(ret == 0)
+    {
+        BOOST_LOGL(app, info) << "Failed at SetCurrentDirectory";
+        return false;
+    }
+
+    ret = ::GetCurrentDirectory(static_cast<DWORD>(v.size()), &v.at(0));
+
+    if(ret == 0)
+    {
+        BOOST_LOGL(app, info) << "Failed at GetCurrentDirectory";
+        return false;
+    }
+
+    return true;
+}
