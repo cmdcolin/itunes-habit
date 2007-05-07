a6f17655da6dc75964ac353a7634fd728b0556ba
colin.diesh <colin.diesh@37d2c1db-bb2f-0410-a901-09a4849b322f>
2007-05-07 01:22:44 +0000

    Moved remotely
    git-svn-id: http://zxcvbn.googlecode.com/svn/trunk/habit@10 37d2c1db-bb2f-0410-a901-09a4849b322f

---
+++ imgCreator.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "dbgUtils.hpp"
+
+#include <tchar.h>
+#include <map>
+#include <string>
+#include <vector>
+
+#include <xercesc/dom/DOMElement.hpp>
+XERCES_CPP_NAMESPACE_USE
+
+class 
+    Creator
+{
+
+public:
+
+    Creator() { BOOST_LOGL(app, info) << __FUNCTION__; }
+    ~Creator() { BOOST_LOGL(app, info) << __FUNCTION__; }
+
+    void createLastPlayedChart(const std::vector<const DOMElement *> & arr, const std::basic_string<TCHAR> & file) const;
+    void createArtistGraph(const std::map<std::wstring, int> & i, const std::basic_string<TCHAR> & file) const;
+    void createDynaLastPlayed(const std::vector<const DOMElement *> & arr, const std::basic_string<TCHAR> & file) const;
+
+private:
+
+};
